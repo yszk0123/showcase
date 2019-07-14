e334bdd143e868863de4f44fd5973fb52de22306
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-07-14 23:47:39 +0900

    feat(sample-preact-redux-saga): introduce typed redux-saga

--- packages/showcase-sample-preact-redux-saga/src/ducks.ts
+++ packages/showcase-sample-preact-redux-saga/src/ducks.ts
@@ -1,12 +1,21 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { call, debounce, put, takeEvery } from "redux-saga/effects";
+
+const DEBOUNCE = 1000;
 
 enum Type {
-  INCREMENT = "INCREMENT"
+  HELLO = "HELLO",
+  INCREMENT = "INCREMENT",
+  INCREMENT_DELAY = "INCREMENT_DELAY",
+  INCREMENT_DEBOUNCE = "INCREMENT_DEBOUNCE"
 }
 
 type Action = { type: Type };
 
+export const hello = () => ({ type: Type.HELLO });
 export const increment = () => ({ type: Type.INCREMENT });
+export const incrementDebounce = () => ({ type: Type.INCREMENT_DEBOUNCE });
 
 const reducer = (state = 0, action: Action) => {
   switch (action.type) {
@@ -17,7 +26,33 @@ const reducer = (state = 0, action: Action) => {
   }
 };
 
+function getMessage(name: string) {
+  return `Hello, ${name}!`;
+}
+
+type Saga<T = unknown> = Generator<unknown, void, T>;
+
+function* helloSaga(): Saga<string> {
+  const message = yield call(getMessage, "world");
+  console.log(message);
+}
+
+function* incrementSaga(): Saga {
+  yield put(increment());
+}
+
+function* incrementDebounceSaga(): Saga {
+  yield debounce(DEBOUNCE, Type.INCREMENT_DEBOUNCE, incrementSaga);
+}
+
+function* saga(): Saga {
+  yield takeEvery(Type.HELLO, helloSaga);
+  yield* incrementDebounceSaga();
+}
+
 export function createAppStore() {
-  const store = createStore(reducer);
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+  sagaMiddleware.run(saga);
   return store;
 }
--- packages/showcase-sample-preact-redux-saga/src/index.tsx
+++ packages/showcase-sample-preact-redux-saga/src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import { render } from "react-dom";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { createAppStore, increment } from "./ducks";
+import { createAppStore, hello, increment, incrementDebounce } from "./ducks";
 
 const mountPoint = document.querySelector(".app");
 const store = createAppStore();
@@ -9,19 +9,28 @@ const store = createAppStore();
 const App = () => {
   const count = useSelector<number, number>(state => state);
   const dispatch = useDispatch();
-  const onClick = useCallback(() => {
+  const onHello = useCallback(() => {
+    dispatch(hello());
+  }, [dispatch]);
+  const onIncrement = useCallback(() => {
     dispatch(increment());
+  }, [dispatch]);
+  const onIncrementDebounce = useCallback(() => {
+    dispatch(incrementDebounce());
   }, [dispatch]);
 
   return (
     <div>
       <p>count {count}</p>
-      <button onClick={onClick}>Increment</button>
+      <button onClick={onHello}>Hello</button>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onIncrementDebounce}>Increment (debounce)</button>
     </div>
   );
 };
 
 render(
+  // @ts-ignore
   <Provider store={store}>
     <App />
   </Provider>,
